4e5aa223aa7ca89db9bba5ae829f8725cf0a3f19
Rocky Munoz <rmunoz@churchcommunitybuilder.com>
2019-12-12 09:29:48 -0700

    applesAndOranges challenge

---
+++ applesAndOranges.js
@@ -0,0 +1,62 @@
+// Go to https://www.hackerrank.com/challenges/apple-and-orange/problem for problem description
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countApplesAndOranges function below.
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    let appleLanding = apples.map((apple) => apple + a);
+    let orangeLanding = oranges.map((orange) => orange + b);
+    
+    let appleHit = appleLanding.filter((apple) => s <= apple & apple <= t );
+    let orangeHit = orangeLanding.filter((orange) => s <= orange & orange <= t );
+    
+    console.log(appleHit.length + '\n' + orangeHit.length);
+}
+
+function main() {
+    const st = readLine().split(' ');
+
+    const s = parseInt(st[0], 10);
+
+    const t = parseInt(st[1], 10);
+
+    const ab = readLine().split(' ');
+
+    const a = parseInt(ab[0], 10);
+
+    const b = parseInt(ab[1], 10);
+
+    const mn = readLine().split(' ');
+
+    const m = parseInt(mn[0], 10);
+
+    const n = parseInt(mn[1], 10);
+
+    const apples = readLine().split(' ').map(applesTemp => parseInt(applesTemp, 10));
+
+    const oranges = readLine().split(' ').map(orangesTemp => parseInt(orangesTemp, 10));
+
+    countApplesAndOranges(s, t, a, b, apples, oranges);
+}
