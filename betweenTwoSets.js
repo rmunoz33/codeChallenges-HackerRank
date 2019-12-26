2b536556294c6a8a084b8f854ac883c186e368b7
Rocky Munoz <rmunoz@churchcommunitybuilder.com>
2019-12-26 08:41:11 -0700

    camelCase challenge

--- betweenTwoSets.js
+++ betweenTwoSets.js
@@ -35,7 +35,79 @@ function readLine() {
 
 function getTotalX(a, b) {
     // Write your code here
-
+    // I KNOW THIS DOESN'T WORK YET. I'LL GET AROUND TO IT LATER.
+    let a = [ 3, 4 ];
+    let b = [ 24, 48 ];
+    let x = [];
+    
+    a.sort(function(a, b){return a-b});
+    b.sort(function(a, b){return a-b});
+    
+    for (let i = 0; i < 100; i++) {
+        for (let j = 0; j < a.length; j++) {
+            if (i%a[j] === 0 && i <= b[0] && x.indexOf(i) < 0) {
+                console.log(i, a[j], (i%a[j] === 0));
+                x.push(i);
+            }
+        }
+    }
+    
+    let p = [];
+    
+    for (let j = 0; j < b.length; j++) {
+        for (let m = 0; m < x.length; m++) {
+            if (b[j]%x[m] === 0) {
+                console.log(x[m]);
+                p.push(x[m]);
+            }
+        }
+    }
+    
+    p.sort(function(a, b){return a-b});
+    
+    let results = [];
+    
+    for (let i = 0; i < p.length - 1; i++) {
+        if (p[i + 1] == p[i]) {
+            results.push(p[i]);
+        }
+    }
+    
+    let fin = [];
+    
+    for (let i = 0; i < results.length; i++) {
+        if (results[i] >= a[a.length - 1] && fin.indexOf(results[i]) < 0) {
+            console.log(results[i]);
+            fin.push(results[i]);
+        }
+    }
+    
+    let end = [];
+    
+    for (let i = 0; i < fin.length; i++) {
+        for (let j = 0; j < a.length; j++) {
+            if (fin[i]%a[j] === 0 && end.indexOf(fin[i]) < 0) {
+                console.log(fin[i]);
+                end.push(fin[i]);
+            }
+        }
+    }
+    
+    let waste = [];
+    
+    for (let j = 0; j < a.length; j++) {
+        for (let e = 0; e < end.length; e++) {
+            // if (end[e]%a[j] === 0) {
+    
+                // waste = end.splice(e, 1); 
+                // delete end[e];
+                console.log(e, end[e], a[j], (end[e]%a[j] === 0));
+            // } 
+        }
+    }
+    
+    console.log(fin.length);
+    return fin.length;
 }
 
 function main() {
---
+++ camelCase.js
@@ -0,0 +1,45 @@
+// Go to https://www.hackerrank.com/challenges/camelcase/problem for problem description
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the camelcase function below.
+function camelcase(s) {
+    let caps = s.replace(/[^A-Z]/g, "").length;
+    return (caps + 1);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    let result = camelcase(s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
